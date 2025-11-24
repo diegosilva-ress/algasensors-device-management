d0c86f726d087d832f213539b184d53e73c967cc
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-11-24 13:24:50 -0300

    implementando get by id

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -1,17 +1,22 @@
 package com.algaworks.algasensors.device.management.api.controller;
 
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
+import com.algaworks.algasensors.device.management.api.model.SensorOutput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
 import com.algaworks.algasensors.device.management.domain.model.SensorID;
 import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api/sensors")
@@ -20,9 +25,17 @@ public class SensorController {
 
   private final SensorRepository sensorRepository;
 
+  @GetMapping("{sensorId}")
+  public SensorOutput getOne(@PathVariable TSID sensorId) {
+    var sensor = sensorRepository.findById(new SensorID(sensorId))
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    return convertToModel(sensor);
+  }
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Sensor create(@RequestBody SensorInput input) {
+  public SensorOutput create(@RequestBody SensorInput input) {
 
     Sensor sensor = Sensor.builder()
         .id(new SensorID(IdGenerator.generateTSID()))
@@ -34,7 +47,20 @@ public class SensorController {
         .enabled(false)
         .build();
 
-    return sensorRepository.saveAndFlush(sensor);
+    sensor = sensorRepository.saveAndFlush(sensor);
+    return convertToModel(sensor);
+  }
+
+  private SensorOutput convertToModel(Sensor sensor) {
+    return SensorOutput.builder()
+        .id(sensor.getId().getValue())
+        .name(sensor.getName())
+        .ip(sensor.getIp())
+        .location(sensor.getLocation())
+        .protocol(sensor.getProtocol())
+        .model(sensor.getModel())
+        .enabled(sensor.getEnabled())
+        .build();
   }
 
 }
