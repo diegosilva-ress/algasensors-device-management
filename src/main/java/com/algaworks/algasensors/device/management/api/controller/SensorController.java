5aa79744c255b6b58d4cede4d11d45f6d4876d70
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-11-24 11:28:07 -0300

    implementando persistencia

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -3,6 +3,9 @@ package com.algaworks.algasensors.device.management.api.controller;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorID;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,21 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
+
+  private final SensorRepository sensorRepository;
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Sensor create(@RequestBody SensorInput sensor) {
+  public Sensor create(@RequestBody SensorInput input) {
 
-    return Sensor.builder()
-        .id(IdGenerator.generateTSID())
-        .name(sensor.getName())
-        .ip(sensor.getIp())
-        .location(sensor.getLocation())
-        .protocol(sensor.getProtocol())
-        .model(sensor.getModel())
+    Sensor sensor = Sensor.builder()
+        .id(new SensorID(IdGenerator.generateTSID()))
+        .name(input.getName())
+        .ip(input.getIp())
+        .location(input.getLocation())
+        .protocol(input.getProtocol())
+        .model(input.getModel())
         .enabled(false)
         .build();
+
+    return sensorRepository.saveAndFlush(sensor);
   }
 
 }
---
+++ src/main/java/com/algaworks/algasensors/device/management/domain/model/SensorID.java
@@ -0,0 +1,37 @@
+package com.algaworks.algasensors.device.management.domain.model;
+
+import io.hypersistence.tsid.TSID;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SensorID implements Serializable {
+
+  private TSID value;
+
+  public SensorID(TSID value) {
+    Objects.requireNonNull(value);
+    this.value = value;
+  }
+
+  public SensorID(Long value) {
+    Objects.requireNonNull(value);
+    this.value = TSID.from(value);
+  }
+
+  public SensorID(String value) {
+    Objects.requireNonNull(value);
+    this.value = TSID.from(value);
+  }
+
+  @Override
+  public String toString() {
+    return value.toString();
+  }
+}
