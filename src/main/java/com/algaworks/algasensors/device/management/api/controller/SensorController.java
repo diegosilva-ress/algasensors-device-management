9e281bd88ea8f6d1403445a20b35f92f6090a30a
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-11-25 11:24:35 -0300

    implementando rest client

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algasensors.device.management.api.controller;
 
+import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.api.model.SensorOutput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
@@ -29,6 +30,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class SensorController {
 
   private final SensorRepository sensorRepository;
+  private final SensorMonitoringClient sensorMonitoringClient;
 
   @GetMapping
   public Page<SensorOutput> search(@PageableDefault Pageable pageable) {
@@ -85,6 +87,8 @@ public class SensorController {
     Sensor sensor = sensorRepository.findById(new SensorID(sensorId))
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     sensorRepository.delete(sensor);
+
+    sensorMonitoringClient.disableMonitoring(sensorId);
   }
 
   @PutMapping("/{sensorId}/enable")
@@ -94,6 +98,8 @@ public class SensorController {
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     sensor.setEnabled(true);
     sensorRepository.save(sensor);
+
+    sensorMonitoringClient.enableMonitoring(sensorId);
   }
 
   @DeleteMapping("/{sensorId}/enable")
@@ -103,6 +109,8 @@ public class SensorController {
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     sensor.setEnabled(false);
     sensorRepository.save(sensor);
+
+    sensorMonitoringClient.disableMonitoring(sensorId);
   }
 
   private SensorOutput convertToModel(Sensor sensor) {
